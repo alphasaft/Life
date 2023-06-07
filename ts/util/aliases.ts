0ba0f348c2bfb4c0f03c87a882cf4aaf09509de7
Alphasaft <morel.hugo.mrlh@gmail.com>
2023-06-07 21:14:27 +0200

    Initial commit

--- ts/util/aliases.ts
+++
@@ -1,2 +0,0 @@
-export type ProgressBar = HTMLProgressElement
-export type Canvas = HTMLCanvasElement
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    entry: ['./src/index.js'],
-    output: {
-      path: __dirname + "/dist", // 'C:/Users/Hugo/Documents/Loisirs/Javascript/life/dist',
-      filename: 'bundle.js'
-    }
-  }
